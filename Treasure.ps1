35305905aa1cd113d01316dc543b6fe2c1e830b3
Celebane <celebane@gmail.com>
2023-10-17 09:23:20 -0700

    Added new item tables
    Updated Treasure.ps1 to handle dice rolls with modifiers and also
    added the ability to roll quantities when when required.

--- Treasure.ps1
+++ Treasure.ps1
@@ -1,6 +1,7 @@
-function UniqueArmor {
+function UniqueArmor
+{
     #Import the top CSV
-    $CSV = Import-Csv ".\Tables\UniqueArmorsMain.csv" -Header ID, Item
+    $CSV = Import-Csv ".\Tables\AllRandomWeapon.csv" -Header ID, Item
 
     #Create the Dice based on the number of rows in the CSV
     #and roll it to see which iten to grab
@@ -9,7 +10,7 @@ function UniqueArmor {
     $AT = $CSV[$Lookup - 1].Item
 
     #Check if there are more items to roll
-    while ($AT -like '*^*')
+    while ($AT -like '*^*^*')
     {
         $Start = $AT.IndexOf('^')
         $End = $AT.IndexOf('^', $AT.IndexOf('^') + 1)
@@ -19,9 +20,43 @@ function UniqueArmor {
         $Die = New-Dice -Sides $CSV.count
         $Lookup = New-DiceRoll -Dice $Die -NoCrits
         $Item = $CSV[$Lookup - 1].Item
-        $AT = $AT.replace('^'+$File+'^', $Item)
+        $AT = $AT.replace('^' + $File + '^', $Item)
     }
 
+    #Check if there are Dice Rolls required
+    while ($AT -like '*##*##*')
+    {
+        $Modifier = 0
+        $Rolls = 0
+        $Start = $AT.IndexOf('##')
+        $End = $AT.IndexOf('##', $AT.IndexOf('##') + 2)
+        $D = $AT.Substring($Start + 2, $End - ($Start + 2))
+        
+        #Check for positive modifier
+        if ($D -like '*+*')
+        {
+            $D = $D -split '+'
+            $Modifier = $Modifier + $D[1]
+            $D = $D -ne $D[1]
+        }
+
+        #Check for positive modifier
+        if ($D -like '*-*')
+        {
+            $D = $D -split '-'
+            $Modifier = $Modifier - $D[1]
+            $D = $D -ne $D[1]
+        }
+
+        $Dice = $D -split "d"
+        for ($i = 0; $i -lt $Dice[0]; $i++)
+        {
+            $Die = New-Dice -Sides $Dice[1]
+            $Roll = New-DiceRoll -Dice $Die -NoCrits
+            $Rolls = $Rolls + $Roll
+        }
+        $AT = $AT.replace('##' + $D + '##', ($Rolls + $Modifier))
+    }
 
     $AT
     
